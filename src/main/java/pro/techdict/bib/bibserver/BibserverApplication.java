a3fdac37bb84f9d87f986ec781ccf5f183eb4233
唐梦予 <tangmengyu.amber@bytedance.com>
2021-07-07 17:17:47 +0800

    fix: remove unused imports.

--- src/main/java/pro/techdict/bib/bibserver/BibserverApplication.java
+++ src/main/java/pro/techdict/bib/bibserver/BibserverApplication.java
@@ -1,9 +1,7 @@
 package pro.techdict.bib.bibserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class BibserverApplication {
